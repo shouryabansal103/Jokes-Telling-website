7c9211fafcd7b0982d8f9581304f4290bf6ed934
shouryabansal103 <bansalshourya092@gmail.com>
2023-10-24 17:22:59 +0530

    Website is uploaded

---
+++ brain.js
@@ -0,0 +1,568 @@
+const jokes = 
+[`Why did the chicken cross the playground?</br>
+To get to the other slide!`,
+
+`Why did the actor fall through the floorboards?</br>
+They were going through a stage!`,
+
+`Why did a scarecrow win a Nobel prize?</br>
+He was outstanding in his field!`,
+
+`Why are peppers the best at archery?</br>
+Because they habanero!`,
+
+`What did the duck say after she bought chapstick?</br>
+Put it on my bill!`,
+
+`Why did an old man fall in a well?</br>
+Because he couldn’t see that well!`,
+
+`What do you call a fake noodle?</br>
+An impasta!`,
+
+`What did the three-legged dog say when he walked into a saloon?</br>
+“I’m looking for the man who shot my paw!”`,
+
+`How do you tell the difference between a bull and a cow?</br>
+It is either one or the udder!`,
+
+`What’s red and smells like blue paint?</br>
+Red paint!`,
+
+`What’s the difference between a hippo and a Zippo?</br>
+One is very heavy, the other is a little lighter!`,
+
+`jokes for kids</br>
+Why can’t you send a duck to space?</br>
+Because the bill would be astronomical!`,
+
+`What does Jeff Bezos do before he goes to sleep?</br>
+He puts his PJ-Amazon!`,
+
+`What happened when the world's tongue-twister champion got arrested?</br>
+They gave him a tough sentence!`,
+
+`What did the mama cow say to the calf?</br>
+It’s pasture bedtime!`,
+
+`How does a vampire start a letter?</br>
+Tomb it may concern!`,
+
+`What do you call an illegally parked frog?</br>
+Toad!`,
+
+`What did one plate say to the other?</br>
+Dinner is on me!`,
+
+`Why do hummingbirds hum?</br>
+Because they don’t know the words!`,
+
+`What do sprinters eat before a race?</br>
+Nothing. They fast!`,
+
+`Two muffins are baking in an oven. One of them looks to the other and says, "Phew, it's getting hot in here!" The other looks back and says, "Ack! A talking muffin!"`,
+
+`Animal Jokes for Kids</br>
+best jokes for kids</br>
+What do cats eat for breakfast?</br>
+Mice Crispies!`,
+
+`What do you call an elephant that doesn’t matter?</br>
+An irrelephant!`,
+
+`What do you get when you cross a rabbit with shellfish?</br>
+An oyster bunny!`,
+
+`Where do polar bears keep their money?</br>
+In a snow bank!`,
+
+`Why did the pony get sent to his room?</br>
+He wouldn’t stop horsing around!`,
+
+`What kind of dog does a magician have?</br>
+A Labracadabrador!`,
+
+`Where do cows go on Friday nights?</br>
+They go to the moo-vies!`,
+
+`Why couldn't the pony sing “Happy Birthday?”</br>
+Because she was just a little hoarse!`,
+
+`How do you make an octopus laugh?</br>
+With ten-tickles!`,
+
+`How do you keep a bull from charging?</br>
+Take away its credit card!`,
+
+`Why can’t a leopard hide?</br>
+Because he’s always spotted!`,
+
+`Advertisement - Continue Reading Below`,
+
+`jokes for kids</br>
+What do frogs order at fast-food restaurants?</br>
+French flies!`,
+
+`What do you get from a pampered cow?</br>
+Spoiled milk!`,
+
+`What do you call an alligator who solves mysteries?</br>
+An investigator!`,
+
+`Why is a snake difficult to fool?</br>
+You can’t pull its leg!`,
+
+`What kind of socks do grizzlies wear?</br>
+None, they have bear feet!`,
+
+`What do you get when you cross a snail with a porcupine?</br>
+A slowpoke!`,
+
+`What did the dog say when it sat on sandpaper?</br>
+"Ruff!"`,
+
+`What's a cat's favorite dessert?</br>
+Chocolate mouse!`,
+
+`What fish only swims at night?</br>
+Starfish!`,
+
+`What does a triceratops sit on?</br>
+Its tricera-bottom!`,
+
+`Knock Knock Jokes for Kids</br>
+jokes for kids</br>
+Knock, knock.</br>
+Who’s there?</br>
+Weekend.</br>
+Weekend who?</br>
+Weekend do anything we want!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Figs.</br>
+Figs who?</br>
+Figs the doorbell, I’ve been knocking forever!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Theodore.</br>
+Theodore who?</br>
+Theodore wasn’t opened so I knocked!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Tank.</br>
+Tank who?</br>
+You’re welcome!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Isabel.</br>
+Isabel who?</br>
+Isabel not working?`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Ice cream.</br>
+Ice cream who?</br>
+ICE CREAM SO YOU CAN HEAR ME!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Icy.</br>
+Icy who?</br>
+Icy you in there!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Dozen.</br>
+Dozen who?</br>
+Dozen anyone want to let me in?`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Scold.</br>
+Scold who?</br>
+Scold outside, let me in!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Water.</br>
+Water who?</br>
+Water you asking so many questions for, just open up!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Cargo.</br>
+Cargo who?</br>
+Car go, “Toot toot, vroom, vroom!”`,
+
+`50 best jokes for kids</br>
+Knock, knock.</br>
+Who’s there?</br>
+Hatch.</br>
+Hatch who?</br>
+Bless you!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Annie.</br>
+Annie who?</br>
+Annie body home?`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Boo.</br>
+Boo who?</br>
+Don’t cry, it’s just a joke!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Lettuce.</br>
+Lettuce who?</br>
+Lettuce in, it’s cold out here!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+An interrupting cow.</br>
+An interrupt—</br>
+MOO!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Owls go.</br>
+Owls go who?</br>
+That’s right!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+A little old lady.</br>
+A little old lady who?</br>
+Hey, I didn’t know you could yodel!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Nana.</br>
+Nana who?</br>
+Nana your business!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Toodle</br>
+Toodle who?</br>
+Toodle-loo!`,
+
+`Knock, knock.</br>
+Who’s there?</br>
+Spell.</br>
+Spell who?</br>
+Okay, W-H-O!`,
+
+`Food Jokes for Kids</br>
+best jokes for kids</br>
+What’s a piece of bread’s least favorite chore?</br>
+Doing a loaf of laundry.`,
+
+`What did the bunny say to the carrot?</br>
+It’s been nice gnawing you!`,
+
+`What do you call a sad strawberry?</br>
+A blueberry!`,
+
+`Why did the tomato blush?</br>
+Because it saw the salad dressing!`,
+
+`What did the pizza say to the topping?</br>
+I never sau-sage a pretty face!`,
+
+`Which vegetable do sailors hate the most?</br>
+Leeks!`,
+
+`What do you call a cheese that’s not yours?</br>
+Nacho cheese!`,
+
+`Why did the cookie go to the nurse?</br>
+Because he felt crummy!`,
+
+`What kind of room doesn’t have doors?</br>
+A mushroom!`,
+
+`What kind of key opens a banana?</br>
+A mon-key!`,
+
+`What happens when a grape gets run over crossing the street?</br>
+A traffic jam!`,
+
+`jokes for kids</br>
+What does garlic do when it gets hot?</br>
+It takes its cloves off!`,
+
+`Where does fruit go on vacation?</br>
+Pear-is!`,
+
+`Why did the melons choose not to get married?</br>
+Because they cantaloupe!`,
+
+`What does a lemon say when it answers the phone?</br>
+Yellow!`,
+
+`What did one dried fruit say when another asked it to the movies?</br>
+It's a date!`,
+
+`Why do bees have sticky hair?</br>
+They use honeycombs!`,
+
+`What does a cow call an earthquake?</br>
+A milkshake!`,
+
+`Why did the man go to the yogurt museum?</br>
+To get a little culture!`,
+
+`Why couldn't the sesame seed climb up the hill?</br>
+Because it was on a roll!`,
+
+`Why can't you trust tacos?</br>
+Because they always spill the beans!`,
+
+`Math Jokes for Kids</br>
+best jokes for kids</br>
+How come no one picks statistics as their favorite subject?</br>
+It’s just average.`,
+
+`Why did two 4s skip dinner?</br>
+Because they already 8!`,
+
+`Did you hear about the mathematician who’s afraid of negative numbers?</br>
+He’ll stop at nothing to avoid them!`,
+
+`What did the triangle feel sorry for the circle?</br>
+Because it’s pointless!`,
+
+`Why was the math book sad?</br>
+It had too many problems!`,
+
+`Did you hear about the rancher who had 97 cows in his field?When he rounded them up, he had 100!`,
+
+`Why couldn’t the angle get a loan?</br>
+Because his parents wouldn’t cosine!`,
+
+`What do you call a number that can’t stay in one place?</br>
+A roamin’ numeral!`,
+
+`Why should you never trust someone writing on graph paper?</br>
+Because they must be plotting something!`,
+
+`What do you call two guys who love math?</br>
+Algebros!`,
+
+`What do you get when you divide a Jack o' lantern by its diameter?</br>
+Pumpkin pi!`,
+
+`50 best jokes for kids</br>
+What did the 90° angle say after an argument?</br>
+"It turns out, I was right!"`,
+
+`What tool do mathematicians use most?</br>
+Multi-pliers!`,
+
+`Why did the student get upset when their teacher called them average?</br>
+It was a mean thing to say!`,
+
+`If a math teacher had four apples in one hand and five apples in the other hand, what would they have altogether?</br>
+Really big hands!`,
+
+`What did the 0 say to the 8?</br>
+Nice belt!`,
+
+`Why was 6 afraid of 7?</br>
+Because 7, 8, 9!`,
+
+`Why should you never start a conversation with pi?</br>
+It'll just go on forever!`,
+
+`What is a bird’s favorite type of math?</br>
+OWL-gebra!`,
+
+`Why do teenagers travel in groups of threes and fives?</br>
+Because they can’t even!`,
+
+`Did you hear about the the circle that kept going to school?</br>
+It has 360°!`,
+
+`Silly Jokes for Kids</br>
+best jokes for kids</br>
+Why did the robber take a bath before he left the scene of the crime?</br>
+He wanted to make a clean getaway!`,
+
+`Why does it take pirates a long time to learn the alphabet?</br>
+Because they can spend years at C!`,
+
+`What did one snowman say to the other snowman?</br>
+It smells like carrots over here!`,
+
+`What did one wall say to the other wall?</br>
+I’ll meet you at the corner!`,
+
+`Why did the kid throw his clock out the window?</br>
+Because he wanted to see time fly!`,
+
+`What do you call a huge pile of cats?</br>
+A meow-ntain!`,
+
+`Why wouldn’t the shrimp share his treasure?</br>
+Because he was a little shellfish!`,
+
+`Why is Cinderella bad at soccer?</br>
+Because she’s always running away from the ball!`,
+
+`What does a rain cloud wear under its clothes?</br>
+Thunderwear!`,
+
+`Why do vampires seem sick all the time?</br>
+Because they’re always coffin!`,
+
+`How do you get an astronaut’s baby to stop crying?</br>
+You rocket!`,
+
+`50 best jokes for kids</br>
+Why are ghosts bad liars?</br>
+Because you can see right through them!`,
+
+`Why are spiders so smart?</br>
+They can find everything on the web!`,
+
+`What are mummies' favorite lunches?</br>
+Wraps!`,
+
+`How does Darth Vader like his toast?</br>
+On the dark side!`,
+
+`Why are mountains so funny?</br>
+They're just hill areas!`,
+
+`Why did the student eat his homework?Because his teacher told him it would be a piece of cake!`,
+
+`What is the Easter bunny’s favorite type of music?</br>
+Hip-hop!`,
+
+`Why did Beethoven get rid of his chickens?</br>
+All they ever said was, “Bach, Bach, Bach!”`,
+
+`Why did the man name his dogs Rolex and Timex?</br>
+Because they were watchdogs!`,
+
+`Why is grass so dangerous?</br>
+Because it’s full of blades!`,
+
+`Puns for Kids</br>
+50 best jokes for kids</br>
+What did the teacher say when a book fell on their head?</br>
+I have only my shelf to blame.`,
+
+`What kind of tree can fit in one hand?</br>
+A palm tree!`,
+
+`How do they keep the basketball arena cool?</br>
+They fill it with fans!`,
+
+`What does a book do to keep warm in the winter?</br>
+It puts on a jacket!`,
+
+`What did the lightbulb say to its sweetheart?</br>
+I wuv you a watt!`,
+
+`What’s the difference between a guitar and a fish?You can tune a guitar, but you can’t tuna fish!`,
+
+`What did the fisherman say to the magician?</br>
+Pick a cod, any cod!`,
+
+`Why did the picture go to prison?</br>
+Because it was framed!`,
+
+`Why do bicycles fall over?</br>
+Because they’re two-tired!`,
+
+`If a seagull flies over the sea, what flies over the bay?</br>
+A bagel!`,
+
+`What does the ocean do when it sees its friends?</br>
+It waves!`,
+
+`jokes for kids</br>
+Why did the golfer wear two pairs of pants?</br>
+Just in case he got a hole in one!`,
+
+`Why was the broom late?</br>
+It over-swept!`,
+
+`What did the paper say to the pencil?</br>
+Write on!`,
+
+`What do you call a belt made of watches?</br>
+A waist of time!`,
+
+`Where do sailboats go when they're sick?</br>
+To the dock!`,
+
+`How does the moon cut his hair?</br>
+Eclipse it!`,
+
+`What do you do when a lemon gets sick?</br>
+You give it lemon-aid!`,
+
+`What’s worse than raining cats and dogs?</br>
+Hailing taxis!`,
+
+`How can you tell if a tree is a dogwood tree?</br>
+By its bark!`,
+
+`What do astronauts do before throwing a party?</br>
+They planet!`,
+
+`Corny Dad Jokes for Kids</br>
+jokes for kids</br>
+What did the family say when they lost 25% of their roof?</br>
+Oof.`,
+
+`Why are elevator jokes so good?</br>
+They work on many levels!`,
+
+`What's worse than finding a worm in your apple?</br>
+Finding half a worm in your apple!`,
+
+`What do you call a boomerang that doesn't come back?</br>
+A stick!`,
+
+`What do you call a bear with no ears?</br>
+A “B!”`,
+
+`What do pirates pay for corn?</br>
+A buck an ear!`,
+
+`Which hand is it better to write with?</br>
+Neither, it’s better to write with a pencil!`,
+
+`What do you call a fish with no eye?</br>
+A fsh!`,
+
+`Why are balloons so expensive?</br>
+Inflation!`,
+
+`What breed of dog can jump higher than a skyscraper?</br>
+Any breed of dog. Skyscrapers can’t jump!`,
+
+`Why can't you trust atoms?</br>
+They make up everything!`]
+
+const len = jokes.length;
+const area = document.getElementById("joke-container");
+const btn = document.getElementById("clickHere-btn")
+const onFirstClick = () => {
+    btn.outerHTML = `<button onclick="onClick()" id="next-btn">Next</button>`;
+    onClick();
+}
+const onClick = () => {
+    let joke_num = Math.floor(Math.random() * len);
+    area.innerHTML = jokes[joke_num];
+}
